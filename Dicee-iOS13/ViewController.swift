4ef4447f032bdf085e1d39c2b00703c5e6ef1d9f
mtotodev05 <mtotodev05@gmail.com>
2020-02-04 21:44:46 +0300

    Update main story board for iPhone SE

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Set the image property
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        // Set the image two property value.
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        // create a dice image array
+        
+        let diceImagesArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageViewOne.image = diceImagesArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceImagesArray[Int.random(in: 0...5)]
+    }
 }
 
